cc43969b1c426e6344778c30bbc4248d00bb9bf6
Sebastian Beimers <sebastianbeimers@gmail.com>
2023-04-03 16:21:06 -0400

    update includes

--- heartwave/device.h
+++ heartwave/device.h
@@ -1,11 +1,18 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
+#include "recording.h"
+#include "log.h"
+#include "menu.h"
 
 class Device
 {
 public:
     Device();
+private:
+    // need one recording
+    // vector(?) of logs, gotta store a few of those
+    // a(?) menu object
 };
 
 #endif // DEVICE_H
